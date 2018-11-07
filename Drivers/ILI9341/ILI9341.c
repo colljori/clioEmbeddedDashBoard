c6da3e888557787eebc0b2ec72e32a8336d43ba3
Joris <joris.collomb@gmail.com>
2018-11-07 19:52:30 +0100

    SPI1 init for ILI9341

--- Drivers/ILI9341/ILI9341.c
+++ Drivers/ILI9341/ILI9341.c
@@ -18,6 +18,8 @@
 */
 
 /* Includes ------------------------------------------------------------------*/
+#include "ILI9341.h"
+#include "stm32f4xx.h"
 /* Externs -------------------------------------------------------------------*/
 /* Private typedef -----------------------------------------------------------*/
 /* Private define ------------------------------------------------------------*/
@@ -33,6 +35,38 @@
  * \param [out]         None
  * -------------------------------------------------------------------------- */
 void ILI9431Init(void){
+  __attribute__((unused)) __IO uint32_t tmpreg = 0x00U;
+  //////////////////////ENABLE CLOCK///////////////
+  /* Activate clock for GPIOA on AHB1 */
+  SET_BIT(RCC->AHB1ENR, RCC_AHB1ENR_GPIOAEN);
+  /* Activate clock for USART2 on APB1 */
+  SET_BIT(RCC->APB2ENR, RCC_APB2ENR_SPI1EN);
+  /* Delay after an RCC peripheral clock enabling */
+  tmpreg = READ_BIT(RCC->APB2ENR, RCC_APB2ENR_SPI1EN);
+  //////////////////////CONF OUTPUT///////////////
+  // GPIOA 5, 6 and 7 in AF mode
+  MODIFY_REG(GPIOA->MODER,
+    GPIO_MODER_MODE7_Msk | GPIO_MODER_MODE6_Msk | GPIO_MODER_MODE5_Msk,
+    GPIO_MODER_MODE6_1   | GPIO_MODER_MODE6_1   | GPIO_MODER_MODE5_1);
+  // GPIOA 5, 6 and 7 AF mode 5 for SPI1
+  MODIFY_REG(GPIOA->AFR[0],
+    GPIO_AFRL_AFSEL7_Msk | GPIO_AFRL_AFSEL6_Msk | GPIO_AFRL_AFSEL5_Msk,
+    GPIO_AFRL_AFSEL7_2 | GPIO_AFRL_AFSEL7_0 |
+    GPIO_AFRL_AFSEL6_2 | GPIO_AFRL_AFSEL6_0 |
+    GPIO_AFRL_AFSEL5_2 | GPIO_AFRL_AFSEL5_0);
+  // GPIOA 5, 6 and 7 in full speed
+  SET_BIT(GPIOA->OSPEEDR,GPIO_OSPEEDR_OSPEED7_0 | GPIO_OSPEEDR_OSPEED7_1 );
+  SET_BIT(GPIOA->OSPEEDR,GPIO_OSPEEDR_OSPEED6_0 | GPIO_OSPEEDR_OSPEED6_1 );
+  SET_BIT(GPIOA->OSPEEDR,GPIO_OSPEEDR_OSPEED5_0 | GPIO_OSPEEDR_OSPEED5_1 );
+  //////////////////////SPI CR1 ///////////////
+  // Select the desired baud rate using the SP1_CR register.
+  // SCK at Fpclk/16 = 90/16 = 5,625MHz FOR TEST PUSPROSE, TEST WITH 24MHz OR EVEN 48MHz.
+  MODIFY_REG(SPI1->CR1, SPI_CR1_BR_Msk, SPI_CR1_BR_1 | SPI_CR1_BR_0);
+  // CPOL = 0 : CK to 0 when idle
+  // CPHA = 0 : The first clock transition is the first data capture edge
+  CLEAR_BIT(SPI1->CR1, SPI_CR1_CPOL | SPI_CR1_CPHA);
+  // Configure the MSTR bit : master mode
+  SET_BIT(SPI1->CR1, SPI_CR1_MSTR);
 }
 
 /* Private functions ---------------------------------------------------------*/
